40714f96d51ba7650362a8faddba1bf281aa7898
Tanguy-ddv <tanguy.dugas01@gmail.com>
2024-10-05 22:33:08 +0200

    new: first commit with init files

---
+++ data/sql-game/localizations.sql
@@ -0,0 +1,13 @@
+INSERT INTO localizations (position, phase_name, language_code, text_value) VALUES
+-- Texts for buttons of the menu
+('LOC_CONNECT', 'menu', 'en_US', 'Log in'),
+('LOC_CONNECT', 'menu', 'fr_FR', 'Se connectet'),
+('LOC_CREATE_PROFIL', 'menu', 'en_US', 'Sign in'),
+('LOC_CREATE_PORFIL', 'menu', 'fr_FR', 'Créer un nouveau profil'),
+('LOC_PLAY_ONLINE', 'menu', 'en_US', 'Play online'),
+('LOC_PLAY_ONLINE', 'menu', 'fr_FR', 'Partie en ligne'),
+('LOC_PLAY_OFFLINE', 'menu', 'en_US', 'Play alone'),
+('LOC_PLAY_OFFLINE', 'menu', 'fr_FR', 'Partie solo'),
+('LOC_MENU_QUIT', 'menu', 'en_US', 'Quit'),
+('LOC_MENU_QUIT', 'menu', 'fr_FR', 'Quitter')
+-- Texts for the lobby
---
+++ data/sql-server/tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE player(
+    player_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+);
